a74bbc8f6369b7b9158b70da52e09f1efcd9e1b7
António Duarte <58123364+antonionduarte@users.noreply.github.com>
2020-10-14 01:05:39 +0100

    Added new exception

---
+++ src/exceptions/ContactDoesNotExistException.java
@@ -0,0 +1,14 @@
+package exceptions;
+
+public class ContactDoesNotExistException extends RuntimeException {
+    
+  /* Constants */
+    private static final long serialVersionUID = 1L;
+    private static final String MSG = "Inexistencia do contacto referido.";
+    
+    /* Constructor */
+    public ContactDoesNotExistException() {
+      super(MSG);
+    }
+  
+}
