f967b6748511563751975e7a9d6869d226293e7f
António Duarte <58123364+antonionduarte@users.noreply.github.com>
2020-10-25 02:00:26 +0000

    Fixed exception

--- src/exceptions/ContactAlreadyExistsException.java
+++ src/exceptions/ContactAlreadyExistsException.java
@@ -4,4 +4,11 @@ public class ContactAlreadyExistsException extends RuntimeException {
 	
 	/* Constants */
 	private static final long serialVersionUID = 1L;
+	private static final String MSG = "Existencia do contacto referido.";
+	
+	/* Constructor */
+	public ContactAlreadyExistsException() {
+		super(MSG);
+	}
+
 }
