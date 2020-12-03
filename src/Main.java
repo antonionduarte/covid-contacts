ebbc81816981325068a6fe849c4b4ad131474ea7
António Duarte <58123364+antonionduarte@users.noreply.github.com>
2020-12-03 03:33:48 +0000

    Tiny change in Main

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,8 @@ import java.util.Scanner;
 
 /**
  * @author Antonio Duarte (58278).
- * @author Goncalo Virginia (56773). This program simulates a simple social network themed around the current COVID-19
+ * @author Goncalo Virginia (56773). 
+ * This program simulates a simple social network themed around the current COVID-19
  * pandemic (let's hope Trump isn't lying about the cure). Users can add others as contacts, create/join groups, post
  * messages (which are sent to all contacts and groups with the main purpose of notifying others if they're infected)
  * and of course, receive messages.
